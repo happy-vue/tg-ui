0dac038988242997fb7f813d7e07c8c5b516c295
SacredWood <2087108700@qq.com>
2022-04-25 22:56:29 +0800

    feat: 移植洪布斯文档-报错

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,11 +1,16 @@
-module.exports = {
+import { defineConfigWithTheme } from 'vitepress'
+import type { Config } from '@vue/theme'
+import { NavbarFix } from './plugins/navbar'
+import Components from 'unplugin-vue-components/vite'
+import { resolve } from 'path'
+
+
+export default defineConfigWithTheme<Config>({
   base: '/',
+  lang: 'zh-CN',
   title: 'tg-ui官方文档',
   description:
     '基于vite为基础搭建的风格脚手架,提供多种模板以便于更高效的解决业务需求！',
-  lang: 'zh-CN',
-  // 所有标题将自动添加anchor链接
-  anchor: true,
   head: [['link', { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }]],
   markdown: {
     // 所有代码块启用行号
@@ -15,4 +20,23 @@ module.exports = {
     // options for markdown-it-toc
     toc: { includeLevel: [1, 2] },
   },
-};
+
+  vite: {
+    define: {
+      __VUE_OPTIONS_API__: false
+    },
+    plugins: [
+      NavbarFix(),
+      Components({
+        dirs: resolve(__dirname, 'theme/components'),
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+        dts: '../.vitepress/components.d.ts',
+        transformer: 'vue3',
+      })
+    ]
+  },
+
+  vue: {
+    reactivityTransform: true
+  }
+});
---
+++ docs/.vitepress/theme/components/HbsButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <a class="hbs-link-button" :href="link" target="_blank">
+    <slot />
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  link: string
+}>()
+</script>
+
+<style scoped>
+.hbs-link-button {
+  display: inline-block;
+  padding: 4px 12px;
+  background-color: var(--vt-c-green);
+  color: #fff !important;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.5s, color 0.5s;
+}
+
+.dark .hbs-link-button {
+  color: var(--vt-c-indigo) !important;
+}
+
+.hbs-link-button:hover {
+  background-color: var(--vt-c-green-dark);
+  transition-duration: 0.2s;
+}
+
+.dark .hbs-link-button:hover {
+  background-color: var(--vt-c-green-light);
+}
+</style>
